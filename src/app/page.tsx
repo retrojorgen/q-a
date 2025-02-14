946b35243a2d95edb2e835ce89459b4f5ca84ed2
Jørgen Jacobsen <jorgeja@gmail.com>
2025-02-14 12:23:16 +0100

    Added accordion to frontpage and text styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,41 @@
-import Image from "next/image";
-import {getAllAccordionItems} from "../lib/api";
-import { AccordionCollection } from "../types"
+import { getAllAccordionItems } from '../lib/api';
+// use contentful instead if you wish, it returns the same data type "AccordionCollection"
+//import { getAccordionData } from "../lib/contentfuljs"
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '../components/ui/accordion';
 
 export default async function Home() {
   const accordionCollection = await getAllAccordionItems();
-  console.log(items);
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      hello
-      {
+  // use contentful instead if you wish, it returns the same data type "AccordionCollection"
+  //const accordionData = await getAccordionData();
 
-      }
+  // using the assumption that there might be multiple accordions in the future
+  return (
+    <div className="max-w-screen-lg mx-auto p-4">
+      {accordionCollection.items?.map((collection, key) => (
+        <section className="w-full" key={key}>
+          <h1 className=" text-heading text-4xl lg:text-7xl font-semibold mb-8">
+            {collection.title}
+          </h1>
+          <div className="text-base lg:text-2xl">
+            <Accordion type="single" collapsible>
+              {collection.accordionItemsCollection?.items?.map(
+                (item, key) => (
+                  <AccordionItem value={'item' + key + 1} key={key}>
+                    <AccordionTrigger>{item.name}</AccordionTrigger>
+                    <AccordionContent>{item.text}</AccordionContent>
+                  </AccordionItem>
+                )
+              )}
+            </Accordion>
+          </div>
+        </section>
+      ))}
     </div>
   );
 }
