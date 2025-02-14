6cdd1366dce7dd4e0fead84de6e93d46af0aeebf
Jørgen Jacobsen <jorgeja@gmail.com>
2025-02-14 12:21:55 +0100

    Updated type to correct array notation and added comment

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,11 +2,16 @@ export interface AccordionItem {
   name: string,
   text: string,
 }
+
+// makes the assumption that there might be more than one accordion in the future?
+
 export interface AccordionCollection {
   items: [
     {
       title: string,
-      accordionItemsCollection: [accordionItem]
+      accordionItemsCollection: {
+        items: [accordionItem]
+      }
     }
   ]
 }
